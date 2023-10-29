ce5456df2c5b43967abbbf7e10a7a8e52da851bc
lucianoz-codes <149193003+lucianoz-codes@users.noreply.github.com>
2023-10-29 11:21:49 -0300

    Add files via upload

---
+++ OD.m
@@ -0,0 +1,57 @@
+function OD_est_value=OD(M,ts,D,lag,wi)
+
+% ------------------------------------------------------------------------
+% Input:
+%         M:   number of multiple times series;
+%         ts:  array of time series (time series can have different lengths L);
+%         wl:  order or embedding dimension (it should be the same for all time series);
+%         lag: vector of lags or embedding delay for the ordinal symbolization, e.g. [1,1,1] if M=3;
+%         wi:  vector of weights for the generalized Jensen-Shannon divergence, e.g. [1/3,1/3,1/3] if M=3;
+% Output:
+%         OD_est_value: ordinal diversity estimated value
+% Example:
+%         OD_randn=OD(100,num2cell(randn(10000,100),1),4,ones(100,1)',1/100*ones(100,1)')
+%         OD estimated for M=100 normal white noises of length L=10^4 data points with order D=4, lags 
+%         equal to 1 and weights equal to 1/M for all time series. 
+% ------------------------------------------------------------------------
+% Reference:
+% If you use this algorithm please cite the following article:
+% 
+% "Quantifying the diversity of multiple time series with an ordinal symbolic approach" 
+% by Luciano Zunino and Miguel C. Soriano, submitted to Phys. Rev. E, 2023
+%
+% Luciano Zunino
+% E-mail: lucianoz@ciop.unlp.edu.ar, luciano.zunino@gmail.com
+% ------------------------------------------------------------------------
+
+p=zeros(factorial(D),M);
+SPE=zeros(M,1);
+for i=1:1:M
+p(:,i)=prob_indices(ts{i},D,lag(i));
+SPE(i,1)=SE(p(:,i));
+end
+OD_est_value=SE(sum(wi.*p,2))-wi*SPE;
+
+function indcs=perm_indices(ts,D,lag)
+m=length(ts)-(D-1)*lag;
+indcs=zeros(m,1);
+for i=1:D-1
+st=ts(1+(i-1)*lag :m+(i-1)*lag);
+for j=i:D-1
+indcs=indcs+(st>ts(1+j*lag:m+j*lag));
+end
+indcs=indcs*(D-i);
+end
+indcs=indcs + 1;
+
+function hist_indcs=hist_indices(ts,D,lag)
+indcs=perm_indices(ts,D,lag);
+hist_indcs=hist(indcs,1:1:factorial(D));
+
+function prob_indcs=prob_indices(ts,D,lag)
+m=length(ts)-(D-1)*lag;
+prob_indcs=hist_indices(ts,D,lag)/m;
+
+function y=SE(P)
+P=P(P~=0);
+y=-sum(P.*log(P));
